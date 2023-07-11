a7eefd130499ef9822678ef9d19451c16f51d38c
ramezHamdiSa'eed <317170274@sha.edu.eg>
2023-07-11 19:21:36 +0300

    player collide using capsuleCast and other cast methods provided by Physics

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -14,10 +14,33 @@ public class Player : MonoBehaviour {
         //! We need to use multiple if not if and else if because the ability to move diagonally like pressing W and D at a time
         //! Actually we need to deal with x and z not x and y but for future refactoring for this code we will separate the IO from player controller
         //Transform.position += (Vector3)inputVector;
+        float moveDistance = moveSpeed * Time.deltaTime;
+        float playerRadius = 0.5f;
+        float playerHeight = 2f;
         Vector2 inputVector = playerInput.GetMovementVectorNormalized();
         Vector3 moveDir = new Vector3(inputVector.x,0,inputVector.y);
         //! To make specific statement frame rate independent we need to multiply by Time.deltaTime or using FixedUpdate as an alternative for Update
-        transform.position +=moveDir*moveSpeed*Time.deltaTime;
+        bool canMove = !Physics.CapsuleCast(transform.position,transform.position+playerHeight*Vector3.up,playerRadius,moveDir,moveDistance);
+        if (!canMove) {
+        Vector3 moveDirX = new Vector3(moveDir.x,0,0).normalized;
+        canMove = !Physics.CapsuleCast(transform.position,transform.position+playerHeight*Vector3.up,playerRadius,moveDirX,moveDistance);
+            if (canMove) {
+                moveDir = moveDirX;
+            }
+            else {
+                Vector3 moveDirZ = new Vector3(0,0,moveDir.z).normalized;
+                canMove = !Physics.CapsuleCast(transform.position,transform.position+playerHeight*Vector3.up,playerRadius,moveDirZ,moveDistance);
+                if (canMove) {
+                    moveDir = moveDirZ;
+                }
+                else { 
+                    // we tried to make the player move right or left when the gamer attempt to move diagonally when collide 
+                }
+            }
+        }
+        if(canMove){
+            transform.position += moveDir * moveDistance;
+        }
         isWalking = moveDir != Vector3.zero;
         float rotationSpeed = 20f;
         //! we need to make the player look at the direction as same as the moveDir but sequentionally or animated with same initial and final speed as the rotationSpeed
--- README.md
+++ README.md
@@ -8,3 +8,4 @@ a 3D game do demonstrate the basics and advanced topics about unity like games n
 - using ready to use animator Controller
 - setup Cinemachine to be above the main camera to ad noise
 - setup medern input system package parallel with the legacy one to do game prototying and production level development
+- make the player collide with other collider objects and how to make the player avoid the collider when the game attempt to move diagonally by make him move horizontally or vertically only
